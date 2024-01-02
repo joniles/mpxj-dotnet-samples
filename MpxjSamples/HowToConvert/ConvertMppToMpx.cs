4a714e84e61d39f6cbe48a8e794c65f50f139be3
joniles <jon.iles@bcs.org.uk>
2024-01-02 14:03:28 +0000

    Add samples

---
+++ MpxjSamples/HowToConvert/ConvertMppToMpx.cs
@@ -0,0 +1,16 @@
+﻿using net.sf.mpxj.mpp;
+using net.sf.mpxj.mpx;
+
+namespace MpxjSamples.HowToConvert;
+
+public class ConvertMppToMpx
+{
+    public void Convert(string inputFile, string outputFile)
+    {
+        var reader = new MPPReader();
+        var projectFile = reader.read(inputFile);
+
+        var writer = new MPXWriter();
+        writer.write(projectFile, outputFile);
+    }
+}
---
+++ MpxjSamples/HowToConvert/ConvertUniversal.cs
@@ -0,0 +1,16 @@
+﻿using net.sf.mpxj.reader;
+using net.sf.mpxj.writer;
+
+namespace MpxjSamples.HowToConvert;
+
+public class ConvertUniversal
+{
+	public void Convert(string inputFile, string outputFile)
+	{
+        var reader = new UniversalProjectReader();
+        var projectFile = reader.read(inputFile);
+
+        var writer = ProjectWriterUtility.getProjectWriter(outputFile);
+        writer.write(projectFile, outputFile);
+    }
+}

2085127310fff0671563d8c9b6e5dec6dabfbc21
Jon Iles <jon.iles@bcs.org.uk>
2024-06-26 16:26:08 +0100

    Update samples

---
+++ MpxjSamples/HowToRead/P3Directory.cs
@@ -0,0 +1,10 @@
+﻿using MPXJ.Net;
+
+public class P3Directory
+{
+    public void Read()
+    {
+        var reader = new UniversalProjectReader();
+        var project = reader.Read("my-p3-directory");
+    }
+}
---
+++ MpxjSamples/HowToRead/P3NamedProject.cs
@@ -0,0 +1,18 @@
+﻿using MPXJ.Net;
+
+public class P3NamedProject
+{
+    public void Read()
+    {
+        // Find a list of the project names
+        var directory = "my-p3-directory";
+        var projectNames = P3DatabaseReader.ListProjectNames(directory);
+
+        // Tell the reader which project to work with
+        var reader = new P3DatabaseReader();
+        reader.ProjectName = projectNames[0];
+
+        // Read the project
+        var project = reader.Read(directory);
+    }
+}

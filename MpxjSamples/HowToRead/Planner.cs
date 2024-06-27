93bd8c4ffc0d6c035406a2d5926491aec5553584
Jon Iles <jon.iles@bcs.org.uk>
2024-06-27 14:54:04 +0100

    Update samples

---
+++ MpxjSamples/HowToRead/Planner.cs
@@ -0,0 +1,10 @@
+﻿using MPXJ.Net;
+
+public class Planner
+{
+    public void Read()
+    {
+        var reader = new UniversalProjectReader();
+        var project = reader.Read("my-samplexml");
+    }
+}

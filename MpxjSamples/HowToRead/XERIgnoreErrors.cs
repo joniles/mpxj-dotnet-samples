068a915f601825a9af61dcbdb2381d09e0394963
Jon Iles <jon.iles@bcs.org.uk>
2024-06-28 13:00:04 +0100

    Update samples

---
+++ MpxjSamples/HowToRead/XERIgnoreErrors.cs
@@ -0,0 +1,12 @@
+﻿using MPXJ.Net;
+
+public class XERIgnoreErrors
+{
+    public void Read()
+    {
+        // TODO enable for MPXJ > 13.0.0
+        //var reader = new PrimaveraXERFileReader();
+        //reader.IgnoreErrors = false;
+        //var project = reader.Read("my-sample.xer");
+    }
+}

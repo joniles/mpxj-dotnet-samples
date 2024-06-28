2f23f43ebaeba40b2e85415781fb8b6523fc506c
Jon Iles <jon.iles@bcs.org.uk>
2024-06-28 15:24:14 +0100

    Update samples

---
+++ MpxjSamples/HowToWrite/MPX.cs
@@ -0,0 +1,11 @@
+﻿using MPXJ.Net;
+
+namespace MPXJ.Samples.HowToWrite;
+
+public class MPX
+{
+    public void Write(ProjectFile project, string fileName)
+    {
+        new UniversalProjectWriter(FileFormat.MPX).Write(project, fileName);
+    }
+}
---
+++ MpxjSamples/HowToWrite/MPXLocale.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using MPXJ.Net;
+
+namespace MPXJ.Samples.HowToWrite;
+
+public class MPXLocale
+{
+    public void Write(ProjectFile project, string fileName)
+    {
+        var writer = new MPXWriter();
+        writer.Culture = CultureInfo.GetCultureInfo("de");
+        writer.Write(project, fileName);
+    }
+}

2d5947ad8b8a59771f969731fe812ce346891f76
joniles <jon@timephased.com>
2026-01-12 15:42:02 +0000

    Add support for Microsoft Planner

--- MpxjSamples/HowToUse/Microsoft/MsPlannerReaderExample.cs
+++ MpxjSamples/HowToUse/Microsoft/MsPlannerReaderExample.cs
@@ -1,21 +1,21 @@
 using System.Linq;
 
-namespace MpxjSamples.HowToUse.Pwa;
+namespace MpxjSamples.HowToUse.Microsoft;
 
 using MPXJ.Net;
 
-public class PwaReaderExample
+public class MsPlannerReaderExample
 {
     public void Read()
     {
-        // The URL for your Project Server instance
-        var projectServerUrl = "https://example.sharepoint.com/sites/pwa";
+        // The URL for your organisation's Dynamics server instance
+        var dynamicsServerUrl = "https://example.api.crm11.dynamics.com";
 
         // We're assuming you have already authenticated as a user and have an access token
         var accessToken = "my-access-token-from-oauth";
 
         // Create a reader
-        var reader = new PwaReader(projectServerUrl, accessToken);
+        var reader = new MsPlannerReader(dynamicsServerUrl, accessToken);
 
         // Retrieve the projects available and print their details
         var projects = reader.GetProjects();

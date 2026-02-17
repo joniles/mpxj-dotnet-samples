4250b64af9560cb912e31ecffc9d68590ee2f633
joniles <jon@timephased.com>
2026-02-17 12:58:05 +0000

    WIP

--- MpxjSamples/CreateTimephased.cs
+++ MpxjSamples/CreateTimephased.cs
@@ -50,31 +50,30 @@ public class CreateTimephased
         // We split this as 20h, 2h and 2h
         //
         var day1 = new TimephasedWork();
-        day1.AmountPerDay = Duration.GetInstance(20, TimeUnit.Hours);
+        day1.AmountPerHour = Duration.GetInstance(60, TimeUnit.Minutes);
         day1.Start = new DateTime(2003, 1, 1, 3, 0, 0);
         day1.Finish = new DateTime(2003, 1, 1, 23, 0, 0);
-        day1.Modified = true;
         day1.TotalAmount = Duration.GetInstance(20, TimeUnit.Hours);
 
         var day2 = new TimephasedWork();
-        day2.AmountPerDay = Duration.GetInstance(2, TimeUnit.Hours);
+        day2.AmountPerHour = Duration.GetInstance(60, TimeUnit.Minutes);
         day2.Start = new DateTime(2003, 1, 2, 8, 0, 0);
         day2.Finish = new DateTime(2003, 1, 1, 10, 0, 0);
-        day2.Modified = true;
         day2.TotalAmount = Duration.GetInstance(2, TimeUnit.Hours);
 
         var day3 = new TimephasedWork();
-        day3.AmountPerDay = Duration.GetInstance(2, TimeUnit.Hours);
+        day3.AmountPerHour = Duration.GetInstance(60, TimeUnit.Minutes);
         day3.Start = new DateTime(2003, 1, 3, 8, 0, 0);
         day3.Finish = new DateTime(2003, 1, 1, 10, 0, 0);
-        day3.Modified = true;
         day3.TotalAmount = Duration.GetInstance(2, TimeUnit.Hours);
 
         //
         // Add the timephased data to the assignment
         //
-        assignment1.TimephasedWork = new TimephasedWork[] { day1, day2, day3 }.ToList();
-
+        assignment1.RawTimephasedRemainingRegularWork.Add(day1);
+        assignment1.RawTimephasedRemainingRegularWork.Add(day2);
+        assignment1.RawTimephasedRemainingRegularWork.Add(day3);
+        
         //
         // Write the file
         //
@@ -82,10 +81,7 @@ public class CreateTimephased
 
         // By default, timephased data is not written so we need to enable it here
         writer.WriteTimephasedData = true;
-
-        // Also, tell the writer not to get clever with our timephased data, just write it as it is...
-        writer.SplitTimephasedAsDays = false;
-
+        
         //
         // If you look at the resulting project in the Resource Usage view in MS Project
         // you should see the work split as 20h, 8h and 8h over the three days of the task.
